493375bc793644263b8ba63d16c7b9348629afa4
Amit Gahlawat <amitgahlawat001@gmail.com>
2025-09-20 22:51:22 +0530

    home path issue resolved

--- src/config/RouterConfig.tsx
+++ src/config/RouterConfig.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { RouteObject } from "react-router-dom";
+import { RouteObject, Navigate } from "react-router-dom";
 import Layout from "../components/layout/Layout";
 import Home from "../pages/Home";
 import About from "../pages/About";
@@ -14,12 +14,20 @@ const routes: RouteObject[] = [
     path: "/",
     element: <Layout />,
     children: [
-      { index: true, path: "/", element: <Home /> },
+      { index: true, element: <Home /> },
+      { path: "door-to-door", element: <Home /> },
+      {
+        path: "door-to-door/",
+        element: <Navigate to="/door-to-door" replace />,
+      },
+
       { path: "about", element: <About /> },
       { path: "services", element: <Services /> },
       { path: "gallery", element: <Gallery /> },
       { path: "contact", element: <Contact /> },
       { path: "trackshipment", element: <Tracking /> },
+
+      // Catch-all 404
       { path: "*", element: <PageNotFound /> },
     ],
   },
